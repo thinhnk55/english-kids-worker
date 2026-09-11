f09362538ce1797bb78ccc95031d371156e30aa1
Khanh Thinh Nguyen <1468695+thinhnk55@users.noreply.github.com>
2026-09-12 06:44:12 +0700

    feat(db): add text_video table to 0002 migration

--- migrations/0002_create_text_audio_and_text_image.sql
+++ migrations/0002_create_text_audio_and_text_image.sql
@@ -16,3 +16,12 @@ CREATE TABLE IF NOT EXISTS text_image (
 );
 
 CREATE INDEX IF NOT EXISTS idx_text_image_texts_id ON text_image(texts_id);
+
+CREATE TABLE IF NOT EXISTS text_video (
+  id TEXT PRIMARY KEY NOT NULL,
+  texts_id TEXT NOT NULL,
+  url TEXT NOT NULL,
+  FOREIGN KEY (texts_id) REFERENCES texts(id) ON DELETE CASCADE
+);
+
+CREATE INDEX IF NOT EXISTS idx_text_video_texts_id ON text_video(texts_id);
